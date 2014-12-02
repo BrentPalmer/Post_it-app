5de766fc190fefc137fd8c5e1468aa7c96a4b175
Brent Palmer <bmpalmer987@gmail.com>
2014-12-02 13:51:09 -0800

    comment,post and user models set

--- db/migrate/20141202190820_create_posts.rb
+++ db/migrate/20141202190820_create_posts.rb
@@ -1,11 +1,9 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.belongs_to :user_id
       t.string :url
       t.string :title
       t.text :description
-
     end
   end
 end
