ef6cd832a59b78a450015b3e1e14aae4363c5d16
Brent Palmer <bmpalmer987@gmail.com>
2014-12-10 16:43:45 -0800

    Before action set in post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,11 @@
 class PostsController < ApplicationController
-  
+  before_action :set_post, only: [:show, :edit, :update]
+
   def index
     @posts = Post.all
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+  def show; end
 
   def new
     @post = Post.new
@@ -24,15 +23,12 @@ class PostsController < ApplicationController
     end
   end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @post = Post.find(params[:id])
 
     if @post.update(post_params)
-      flash[:notice] = "The post was successfully edited"
+      flash[:notice] = "The post was successfully edited."
       redirect_to posts_path(@post)
     else
       render :edit
@@ -43,5 +39,9 @@ private
 
   def post_params
     params.require(:post).permit(:title, :url, :description)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
