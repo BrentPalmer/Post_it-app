d3bde497df0fc244609aea51ef0a0adae379943a
Brent Palmer <bmpalmer987@gmail.com>
2014-12-18 12:33:55 -0800

    Added User controller and Register new user form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Account as been created, you are now logged in."
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+private
+   
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ PostitTemplate::Application.routes.draw do
     resources :comments, only: [:create]
   end
   resources :categories, only: [:new, :create, :show]
+  resources :users
 end
