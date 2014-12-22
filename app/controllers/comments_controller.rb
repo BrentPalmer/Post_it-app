fdb38cec930fe1ade3772e479d6dd3672047908e
Brent Palmer <bmpalmer987@gmail.com>
2014-12-22 14:00:10 -0800

    Added voting on posts and comments, limitations for 1 vote per post or comment and limitations for logged in vs not logged in

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,10 +6,23 @@ class CommentsController < ApplicationController
     @comment.creator = current_user
 
     if @comment.save
-      flash[:notice] = "Your comment was successfully added"
+      flash[:notice] = "Your comment was successfully added."
       redirect_to post_path(@post)
     else
       render 'posts/show'
     end
+  end
+
+  def vote
+    comment = Comment.find(params[:id])
+    vote = Vote.create(voteable: comment, creator: current_user, vote: params[:vote])
+    
+    if vote.valid?
+      flash[:notice] = "Your vote was counted."
+    else
+      flash[:error] = "You can only vote on a comment once."
+    end
+
+    redirect_to :back
   end
 end
