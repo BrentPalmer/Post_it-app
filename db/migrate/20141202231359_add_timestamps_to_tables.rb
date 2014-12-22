27a56807e9b9b8301fe0a2b84de40649b2cb7535
Brent Palmer <bmpalmer987@gmail.com>
2014-12-22 14:38:51 -0800

    change of migration timestamp file

--- db/migrate/20141202231359_add_timestamps_to_tables.rb
+++ db/migrate/20141202231359_add_timestamps_to_tables.rb
@@ -1,7 +1,5 @@
 class AddTimestampsToTables < ActiveRecord::Migration
   def change
-    add_column :posts, :created_at, :datetime
-    add_column :posts, :updated_at, :datetime
     add_column :users, :created_at, :datetime
     add_column :users, :updated_at, :datetime
     add_column :comments, :created_at, :datetime
