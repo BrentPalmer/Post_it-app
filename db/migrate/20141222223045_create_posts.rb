3a8b5d3d8fbeacc5651335968e170ffb195f3a5a
Brent Palmer <bmpalmer987@gmail.com>
2014-12-22 14:33:08 -0800

    Changed Posts migration by accident, had to create new migration

--- db/migrate/20141222223045_create_posts.rb
+++ db/migrate/20141222223045_create_posts.rb
@@ -3,6 +3,8 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :url, :title
       t.text :description
+
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141220004917) do
+ActiveRecord::Schema.define(version: 20141222223045) do
 
   create_table "categories", force: true do |t|
     t.string "name"
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 20141220004917) do
     t.string   "url"
     t.string   "title"
     t.text     "description"
-    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
